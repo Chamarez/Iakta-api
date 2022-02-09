d42b729e7c3a5d86a71d37f60d936dbffc223e39
Chamarez <michel.chamarez@gmail.com>
2022-02-09 05:23:49 +0100

    feat:enviorment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+
+
+
 
   const options = new DocumentBuilder()
     .setTitle('Iakta Challenge')
@@ -16,7 +22,17 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+    await app.listen(config.get('APP_PORT'), '0.0.0.0', () =>
+    console.log(
+      `service ${config.get(
+        'npm_package_name',
+      )} is listening on port ${config.get(
+        'APP_PORT',
+      )} with prefix path '/${config.get(
+        'APP_PREFIX',
+      )} in the environment ${config.get('NODE_ENV')} `,
+      'AppStart',
+    ),
+  );
 }
 bootstrap();
